a15e46fee3d8cac300931bafa5e7ec3a987c6f77
LLewis <lorimlewis@gmail.com>
2018-12-19 23:08:37 +0000

    Add files via upload

--- app/table/table.component.ts
+++ app/table/table.component.ts
@@ -5,6 +5,23 @@ import { DataTablesModule } from 'angular-datatables';
 
 import { AppComponent } from './app.component';
 
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
+}
+
+@Component({
+  selector: 'AppComponent',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+
 @NgModule({
   declarations: [
     AppComponent
@@ -17,4 +34,37 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+
+export class AppComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  persons: Person[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const that = this;
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2,
+      serverSide: true,
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        that.http
+          .post<DataTablesResponse>(
+            'https://angular-datatables-demo-server.herokuapp.com/',
+            dataTablesParameters, {}
+          ).subscribe(resp => {
+            that.temps = resp.data;
+
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: []
+            });
+          });
+      },
+      columns: [{ data: 'id' }, { data: 'temp' }, { data: 'city' }]
+    };
+  }
+}
